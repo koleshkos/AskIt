b0d20905d17d49ea91b0862ff39c3a3584d11d54
Sergey <koleshkoser@gmail.com>
2021-07-13 23:48:10 +0300

    fixed bug with show comment for question

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -8,7 +8,7 @@ class AnswersController < ApplicationController
       flash[:success] = "Anser created!"
       redirect_to question_path(@question)
     else
-      @answers = Answer.order created_at: :desc
+      @answers = @question.answers.order created_at: :desc
       render 'questions/show'
     end
   end
