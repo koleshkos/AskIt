2cecbde5af33320b36c072b6ff26e795a74c7840
Sergey <koleshkoser@gmail.com>
2021-07-18 23:05:37 +0300

    replacing kaminari with pagy

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,7 +3,7 @@ class QuestionsController < ApplicationController
   
   def show
     @answer = @question.answers.build
-    @answers = @question.answers.order(created_at: :desc).page(params[:page])
+    @pagy, @answers = pagy @question.answers.order(created_at: :desc)
     #@answers = Answer.where(question: @question).limit(2).order created_at: :desc # limit ограничивает количество выдаваемых ответов. в нашем случае 2 первых
   end
 
@@ -26,7 +26,7 @@ class QuestionsController < ApplicationController
   end
 
   def index
-    @questions = Question.order(created_at: :desc).page params[:page]
+    @pagy, @questions = pagy Question.order(created_at: :desc)
   end
 
   def new
